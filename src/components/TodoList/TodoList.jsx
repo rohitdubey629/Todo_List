6e635de3ae6bae0f0dadf52dd2e723c6a9970e01
rohitdubey629 <dubeyrohit629@gmail.con>
2023-09-02 11:28:46 +0530

    todo action methord difined in outside

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,6 +4,31 @@ import TodoContext from "../context/TodoContext";
 
 const TodoList = () => {
   const { list, setList } = useContext(TodoContext);
+
+  function onFinished(todo, isFinished) {
+    const updateList = list.map((t) => {
+      if (t.id === todo.id) {
+        todo.finished = isFinished;
+      }
+      return t;
+    });
+    return setList(updateList);
+  }
+
+  function onDelete(todo) {
+    setList([...list].filter(({ id }) => id !== todo.id));
+  }
+
+  function onEdit(todo, todoText) {
+    const updateList = list.map((t) => {
+      if (t.id === todo.id) {
+        todo.todoData = todoText;
+      }
+      return t;
+    });
+    return setList(updateList);
+  }
+
   return (
     <div>
       {list.length > 0 &&
@@ -13,27 +38,9 @@ const TodoList = () => {
             id={todo.id}
             isFinished={todo.finished}
             todoData={todo.todoData}
-            changeFinished={(isFinished) => {
-              const updateList = list.map((t) => {
-                if (t.id === todo.id) {
-                  todo.finished = isFinished;
-                }
-                return t;
-              });
-              return setList(updateList);
-            }}
-            deleteItem={() => {
-              setList([...list].filter(({ id }) => id !== todo.id));
-            }}
-            onEdit={(todoText) => {
-              const updateList = list.map((t) => {
-                if (t.id === todo.id) {
-                  todo.todoData = todoText;
-                }
-                return t;
-              });
-              return setList(updateList);
-            }}
+            changeFinished={(isFinished) => onFinished(todo, isFinished)}
+            deleteItem={() => onDelete(todo)}
+            onEdit={(todoText) => onEdit(todo, todoText)}
           ></Todo>
         ))}
     </div>
